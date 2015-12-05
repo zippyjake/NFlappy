7923919e9bca8e2794a4b53fa94110b8d5180e8e
Jake Seeber <zippyjake1@gmail.com>
2015-12-05 11:52:13 -0500

    save

--- www/js/boot.js
+++ www/js/boot.js
@@ -3,12 +3,14 @@
 'use strict';
 
 var Flappy = { };
-
+'
 var Flappy.Boot = function () {}
 
 Flappy.Boot.prototype = {
 
   init: function () {
+    console.log("%c~~~ Flappy Bird ~~~/n",
+               "color:#FF0000");
   };
 
   preload: function () {
